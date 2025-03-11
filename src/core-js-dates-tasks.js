eff726b19285b1e295c417b0e8cbff030b79deba
Aleksandr Mingalev <amingalev@gmail.com>
2025-03-11 01:00:22 +0100

    fix: fix variables

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,11 +32,11 @@ function dateToTimestamp(date) {
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
 function getTime(date) {
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const hr = String(date.getHours()).padStart(2, '0');
+  const min = String(date.getMinutes()).padStart(2, '0');
+  const sec = String(date.getSeconds()).padStart(2, '0');
 
-  return `${hours}:${minutes}:${seconds}`;
+  return `${hr}:${min}:${sec}`;
 }
 
 /**
